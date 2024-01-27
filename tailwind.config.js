08e59b91a3bf6d43134ac1319c2069be358326c1
Pavan Kumar <pavankittu19@gmail.com>
2024-01-27 11:19:01 +0530

    adjust news/id page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+        teal: colors.teal,
+      },
+    },
+  },
+  plugins: [],
+}
+
